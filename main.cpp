ee933dd6bcf2b0959780c9a99916116ff41a14a7
Håvard Skordal Thorsen <haavardsth@uia.no>
2021-02-05 19:10:28 +0100

    Removed redundant code in the main() function. Added example usage in main(). Added some errors to the function. FIXME: Add conversion for negative ranges!

--- main.cpp
+++ main.cpp
@@ -1,48 +1,39 @@
 #include <iostream>
 
-//Convert the value range into a value between 0 and 255.
-double convertRange(int oldMin, int oldMax, int oldValue){
-  //Variables for the new range min and max values
-  int newMin = 0;
-  int newMax = 255;
+//Convert a range to a new range
+//FIXME: Add conversion for negative ranges
+double convertRange(double valueToConvert, double oldMin, double oldMax, double newMin, double newMax){
 
-  //Variables for the ranges
-  //Using doubles to avoid integer division (which = 0)
-  double oldRange = oldMax - oldMin;
-  double newRange = newMax - newMin;
+    //Errors
+    if(oldMin == oldMax){
+        std::cout << "Input range is zero!" << std::endl;
+        return 0;
+    }
+    if(newMin == newMax){
+        std::cout << "Output range is zero!" << std::endl;
+        return 0;
+    }
+    if(oldMin == newMin && oldMax == newMax){
+        std::cout << "The ranges are equal, no conversion needed!" << std::endl;
+        return valueToConvert;
+    }
+    if(valueToConvert < oldMin || valueToConvert > oldMax){
+        std::cout << "Value to convert out of range!" << std::endl;
+        return -1;
+    }
 
-  double newValue = (oldValue - oldMin) * (newRange / oldRange) + newMin;
+    //Declare variables
+    double oldRange = oldMax - oldMin;
+    double newRange = newMax - newMin;
+    double newValue = (valueToConvert - oldMin) * (newRange / oldRange) + newMin;
 
-  return newValue;
+    return newValue;
 }
 
-int main() {
-    int oldMin, oldMax, oldVal, newMin, newMax;
-    double oldRange, newRange;
-    double newValue;
+int main(){
 
-    std::cout << "This program converts one range to another range\n\n";
-
-    std::cout << "\nOld range lowest limit: ";
-    std::cin >> oldMin;
-    std::cout << "\nOld range max limit: ";
-    std::cin >> oldMax;
-    std::cout << "\nCurrent value: ";
-    std::cin >> oldVal;
-    std::cout << "\nNew range lowest limit";
-    std::cin >> newMin;
-    std::cout << "\nNew range max limit";
-    std::cin >> newMax;
-
-    oldRange = oldMax - oldMin;
-    newRange = newMax - newMin;
-
-    newValue = (oldVal - oldMin) * (newRange / oldRange) + newMin;
-
-    std::cout << "The old value: " << oldVal << " in the range from " << oldMin << " to " << oldMax << " is...\n"
-    << newValue << " in the new range from " << newMin << " to " << newMax << std::endl;
-
-    std::cout << "\n\nPress any key to exit";
+    //Example: Convert a value from a 10 bit sensor into an 8 bit analog value
+    std::cout << convertRange(500, 0, 1023, 0, 255);
 
     return 0;
 }
